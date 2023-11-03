b0d22ccea80a3d2d50b8815f762a688a3e7aae44
Edy Susilo <masedysu69@gmail.com>
2023-11-04 04:15:28 +0700

    rendering list

--- src/Components/Fragments/CardProducts.jsx
+++ src/Components/Fragments/CardProducts.jsx
@@ -3,7 +3,7 @@ import Button from "../Elements/Button/Index";
 const CardProduct = (props) => {
     const { children } = props
     return (
-        <div className="w-full max-w-sm bg-slate-100 border border-gray-200 rounded-lg shadow mx-2">
+        <div className="w-full max-w-sm bg-slate-100 border border-gray-200 rounded-lg shadow mx-2 flex flex-col justify-between">
             {children}
         </div>
     )
@@ -19,21 +19,23 @@ const Header = (props) => {
 }
 
 const Body = (props) => {
-    const { tittle, children } = props
+    const { name, children } = props
     return (
-        <div className="px-5 pb-3">
+        <div className="px-5 pb-5 py-auto h-full">
             <a href="#">
-                <h5 className="text-xl font-semibold tracking-tight text-gray-900">{tittle}</h5>
-                <p className="text-gray-700 mb-2">$64.00</p>
+                <h5 className="text-xl font-semibold tracking-tight text-gray-900">{name}</h5>
+                
                 <p className="text-gray-700 mb-2">{children}</p>
             </a>
         </div>
     )
 }
 
-const Footer = () => {
+const Footer = (props) => {
+    const {price} = props
     return (
-        <div className="px-5 pb-5">
+        <div className="flex items-center justify-between p-5">
+            <span className="text-gray-700 mb-2">{price}</span>
             <Button className="bg-blue-600 w-full">Add to cart</Button>
         </div>
     )
--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -1,22 +1,43 @@
 import Button from "../Components/Elements/Button/Index"
 import CardProduct from "../Components/Fragments/CardProducts"
+
+const product = [
+    {
+        id : 1,
+        image : "/images/shoes1.jpeg",
+        name : "Running shoes",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price : "$64.00",
+    },
+    {
+        id : 2,
+        image : "/images/shoes1.jpeg",
+        name : "Running shoes 2",
+        description : "lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price : "$72.00",
+    },
+    {
+        id : 2,
+        image : "/images/shoes1.jpeg",
+        name : "Running shoes 3",
+        description : "lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price : "$75.00",
+    },
+]
+
 const ProductsPage = () => {
     return (
-        <div className="flex justify-center min-h-screen items-center">
+        <div className="flex justify-center py-5">
+            {product.map((product) => (
             <CardProduct>
-                <CardProduct.Header image="/images/shoes1.jpeg"></CardProduct.Header>
-                <CardProduct.Body tittle="Running shoes">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus saepe, earum eum dolor nostrum ducimus dolores inventore repellendus ut explicabo vero fuga quae animi enim iure beatae, labore consequuntur molestiae.
+                <CardProduct.Header image={product.image}></CardProduct.Header>
+                <CardProduct.Body name={product.name}>
+                    {product.description} 
                 </CardProduct.Body>
-                <CardProduct.Footer />
+                <CardProduct.Footer price={product.price} />
             </CardProduct>
-            <CardProduct>
-                <CardProduct.Header image="/images/shoes1.jpeg"></CardProduct.Header>
-                <CardProduct.Body tittle="Running shoes">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus saepe, earum eum dolor nostrum ducimus dolores inventore repellendus ut explicabo vero fuga quae animi enim iure beatae, labore consequuntur molestiae.
-                </CardProduct.Body>
-                <CardProduct.Footer />
-            </CardProduct>
+                
+            ))}
         </div>
     )
 }
